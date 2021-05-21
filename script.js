ba896a0cc58fa1b75c7ab2b124482c6b653758fd
Adriel Dias <you@example.com>
2021-05-21 20:35:45 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//1
+let itens = [`iten1`, `iten2`, `iten3`, `iten4`, `iten5`]
+console.log(itens)
+
+//2
+itens.unshift(`extraIten`)
+console.log(itens)
+
+//3
+itens.pop()
+console.log(itens)
+
+//4
+itens.push(`itenS+`, `itenS++`)
+console.log(itens)
+
+//5
+itens.shift()
+console.log(itens)
+
+//6
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
